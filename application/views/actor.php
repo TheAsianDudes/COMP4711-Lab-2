709b6548fe0769a00a69cc872427455474201665
InochiTanaka <yunominosencha@gmail.com>
2018-01-19 22:34:36 -0800

    #8, implement the function to show each actors' quotes, name, and role on each page Signed-off-by:InochiTanaka <yunominosencha@gmail.com>

--- application/views/actor.php
+++ application/views/actor.php
@@ -4,4 +4,15 @@
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 </a>
         </div>
+    <div class="quote">
+        <p><font size="6" >"{what}"</font></p>
+        <br>
+    </div>
+    
+    <div class="info">
+        <font size="5" >
+            <p>Name: {who}</p>
+            <p>Role: {role}</p>
+        </font>
+    </div>
 </div>
